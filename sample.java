2ada0808475aeaecbe89b7e30a2c5adfce230f68
Al <aldelossantos23@gmail.com>
2019-08-26 17:46:20 +0800

    asd

--- sample.java
+++ sample.java
@@ -1,8 +1,20 @@
-Helloworld.java
-public class Helloworld{
+
+public class sample{
 
 	public static void main(String[] args) {
 		System.out.println("Hello mga tanga");
+		String ss = "tanga tanga";
+		System.out.println(ss);
+
+		if (ss == "asd") {
+			System.out.println("uto");
+			
+		}
+		else{
+			System.out.println("nc1");
+		}
+		
+
 		
 	}
 }
